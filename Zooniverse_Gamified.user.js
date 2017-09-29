794145e336d6c92a6ce7dba8b76f589819349511
Giuliano <delapenna.g@gmail.com>
2017-09-29 13:51:32 -0300

    Saca cosas que estaban al pedo

--- Zooniverse_Gamified.user.js
+++ Zooniverse_Gamified.user.js
@@ -8,31 +8,34 @@
 //window.addEventListener("load", function (){
 
   console.log("Welcome to Zooniverse Gamified!");
-  var awaiting = setInterval(function() {
-    var classifierElement = document.querySelector('.classifier');
-    if (classifierElement != undefined) {
-      clearInterval(awaiting);
-      //Llamar a lo de abajo una vez que sé que cargó el elemento
-    }
-    console.log("DALEs " , classifierElement);
-  }, 1000);
-  //"use strict";
-  
-  //var tt;
-  //tt = window.location.href;
-  //console.log("What? : " + tt);
   if ((window.location.href.indexOf('classify') > -1)) {
-    console.log("Found classify element");
-    
-    lol = document.getElementsByClassName('classifier');
-    console.log("DALEs " + lol[0]);
-    document.getElementsByClassName('classifier')[0].addEventListener("load", addScoreboardDiv);
-    function addScoreboardDiv() {
-      console.log("Loaded classifier element");
+	  var awaiting = setInterval(function() { //Se podrá hacer con Promise?
+	    var classifierElement = document.querySelector('.classifier');
+	    if (classifierElement != undefined) {
+	      clearInterval(awaiting);
+	      addScoreboard();
+	      //Llamar a lo de abajo una vez que sé que cargó el elemento
+	    }
+	  }, 100);
+	  //"use strict";
+	  
+	  //var tt;
+	  //tt = window.location.href;
+	  //console.log("What? : " + tt);
+	  function addScoreboard() {
+
+	    console.log("Found classify element");
+	    
+	    //lol = document.getElementsByClassName('classifier');
+	    //console.log("DALEs " + lol[0]);
+	    //document.getElementsByClassName('classifier')[0].addEventListener("load", addScoreboardDiv);
+	    //function addScoreboardDiv() {
+	    //  console.log("Loaded classifier element");
       var scoreboardDiv = document.createElement('div');
-      scoreboardDiv.appendChild(document.createTextNode('Testing'));
+      scoreboardDiv.appendChild(document.createTextNode('Testing')); //Meter una tabla acá
       document.getElementsByClassName('task-area')[0].parentNode.insertBefore(scoreboardDiv, document.getElementsByClassName('task-area')[0].nextSibling);
-    }
-  }
+	   // }
+	  }
+}
 //});
 
