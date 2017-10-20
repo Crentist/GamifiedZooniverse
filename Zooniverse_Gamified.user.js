46df04af9e44be2ab9c8896e69aa63ec1eb7a7d8
Giuliano De La Penna <delapenna.g@gmail.com>
2017-10-20 12:11:30 -0300

    Formateo del código

--- Zooniverse_Gamified.user.js
+++ Zooniverse_Gamified.user.js
@@ -1,4 +1,4 @@
-// ==UserScript==
+﻿// ==UserScript==
 // @name        Zooniverse Gamified
 // @namespace   colaboratorio
 // @description Adds a layer of gamification to Zooniverse
@@ -8,81 +8,75 @@
 //window.addEventListener("load", function (){
 
   console.log("Welcome to Zooniverse Gamified!");
-  if ((window.location.href.indexOf('classify') > -1)) {
-	  var awaiting = setInterval(function() { //Se podrá hacer con Promise?
-	    var classifierElement = document.querySelector('.classifier');
-	    if (classifierElement != undefined) {
-	      clearInterval(awaiting);
-	      addScoreboard();
-	    }
-	  }, 100);
-	  function addScoreboard() {
-	    //var styleLink = document.createElement("LINK");
-	    //styleLink.setAttribute("rel", "stylesheet");
-	    //styleLink.setAttribute("type", "text/css");
-	    //styleLink.setAttribute("href", "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css");
-	    //document.head.appendChild(styleLink);
+	if ((window.location.href.indexOf('classify') > -1)) {
+		var awaiting = setInterval(function() { //Se podrá hacer con Promise?
+			var classifierElement = document.querySelector('.classifier');
+			if (classifierElement != undefined) {
+				clearInterval(awaiting);
+				addScoreboard();
+			}
+		}, 100);
+		
+		function addScoreboard() {
+
+			var tableStyle = `element {
+							}
+							.table > caption + thead > tr:first-child > td, .table > caption + thead > tr:first-child > th, .table > colgroup + thead > tr:first-child > td, .table > colgroup + thead > tr:first-child > th, .table > thead:first-child > tr:first-child > td, .table > thead:first-child > tr:first-child > th {
+							    border-top: 0;
+							}
+							.table > thead > tr > th {
+							    vertical-align: bottom;
+							    border-bottom: 2px solid #ddd;
+							}
+							.table > tbody > tr > td, .table > tbody > tr > th, .table > tfoot > tr > td, .table > tfoot > tr > th, .table > thead > tr > td, .table > thead > tr > th {
+							    padding: 8px;
+							    line-height: 1.42857143;
+							    vertical-align: top;
+							    border-top: 1px solid #ddd;
+							}
+							table {
+							    border-spacing: 0;
+							    border-collapse: collapse;
+							}`;
+
+			var style = document.createElement("style");
+			style.type = "text/css";
+			style.appendChild(document.createTextNode(tableStyle));
+			document.head.appendChild(style);
+
+			console.log("Found classify element");
 
 
-
-	  	var tableStyle = `element {
-												}
-												.table > caption + thead > tr:first-child > td, .table > caption + thead > tr:first-child > th, .table > colgroup + thead > tr:first-child > td, .table > colgroup + thead > tr:first-child > th, .table > thead:first-child > tr:first-child > td, .table > thead:first-child > tr:first-child > th {
-												    border-top: 0;
-												}
-												.table > thead > tr > th {
-												    vertical-align: bottom;
-												    border-bottom: 2px solid #ddd;
-												}
-												.table > tbody > tr > td, .table > tbody > tr > th, .table > tfoot > tr > td, .table > tfoot > tr > th, .table > thead > tr > td, .table > thead > tr > th {
-												    padding: 8px;
-												    line-height: 1.42857143;
-												    vertical-align: top;
-												    border-top: 1px solid #ddd;
-												}
-												table {
-												    border-spacing: 0;
-												    border-collapse: collapse;
-												}`;
-
-	    var style = document.createElement("style");
-      style.type = "text/css";
-      style.appendChild(document.createTextNode(tableStyle));
-      document.head.appendChild(style);
-
-	    console.log("Found classify element");
-
-
- 			var containerDiv = document.getElementsByClassName('field-guide-pullout-toggle')[0].parentNode;
- 			var node = document.createElement("strong");               
+			var containerDiv = document.getElementsByClassName('field-guide-pullout-toggle')[0].parentNode;
+			var node = document.createElement("strong");               
 			var textnode = document.createTextNode("Puntoh");        
 			node.appendChild(textnode);  
-	    var botonScore = document.createElement('button');
-	    botonScore.type = 'button';
-	    botonScore.className = 'field-guide-pullout-toggle';
-	    botonScore.appendChild(node);
-	    containerDiv.insertBefore(botonScore, document.getElementsByClassName('field-guide-pullout-toggle')[0].nextSibling);
-	    //Esto por ahí sirve para algo. Agrega un botón que despliega, al costado. Funciona solamente con proyectos que andan, creo, que tienen 'Field Guide'. Por ahí garpa poner los puntos ahí, para que no moleste en otro lado.
+			var botonScore = document.createElement('button');
+			botonScore.type = 'button';
+			botonScore.className = 'field-guide-pullout-toggle';
+			botonScore.appendChild(node);
+			containerDiv.insertBefore(botonScore, document.getElementsByClassName('field-guide-pullout-toggle')[0].nextSibling);
+			//Esto por ahí sirve para algo. Agrega un botón que despliega, al costado. Funciona solamente con proyectos que andan, creo, que tienen 'Field Guide'. Por ahí garpa poner los puntos ahí, para que no moleste en otro lado.
 
 
-      var scoreboardDiv = document.createElement('div');
-      var scoreboardTable = document.createElement("table");
-      scoreboardTable.className = 'table';
-      scoreboardTable.id = "tablaPuntaje";
-      console.log("Table created");
-      var row = scoreboardTable.insertRow();
-      row.style.backgroundColor = "#43bbfd";
-      var nickCell = row.insertCell(0);
-      var scoreCell = row.insertCell(-1);
-      var row2 = scoreboardTable.insertRow();
-      var nickCell2 = row2.insertCell(0);
-      var scoreCell2 = row2.insertCell(-1);
-      nickCell.innerHTML = "Nickname";
-      scoreCell.innerHTML = "Score";
+			var scoreboardDiv = document.createElement('div');
+			var scoreboardTable = document.createElement("table");
+			scoreboardTable.className = 'table';
+			scoreboardTable.id = "tablaPuntaje";
+			console.log("Table created");
+			var row = scoreboardTable.insertRow();
+			row.style.backgroundColor = "#43bbfd";
+			var nickCell = row.insertCell(0);
+			var scoreCell = row.insertCell(-1);
+			var row2 = scoreboardTable.insertRow();
+			var nickCell2 = row2.insertCell(0);
+			var scoreCell2 = row2.insertCell(-1);
+			nickCell.innerHTML = "Nickname";
+			scoreCell.innerHTML = "Score";
 			nickCell2.innerHTML = "lomejor";
-      scoreCell2.innerHTML = "100";
-      scoreboardDiv.appendChild(scoreboardTable); //Meter una tabla acá
-      console.log("Appended table to div");	
-      document.getElementsByClassName('task-area')[0].parentNode.insertBefore(scoreboardDiv, document.getElementsByClassName('task-area')[0].nextSibling);
-	  }
+			scoreCell2.innerHTML = "100";
+			scoreboardDiv.appendChild(scoreboardTable); //Meter una tabla acá
+			console.log("Appended table to div");	
+			document.getElementsByClassName('task-area')[0].parentNode.insertBefore(scoreboardDiv, document.getElementsByClassName('task-area')[0].nextSibling);
+		}
 }
